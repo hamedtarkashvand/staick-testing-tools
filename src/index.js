2793fc432df93e69b8d1b145f884e595f9f8e6a6
hamedtarkashvand <hamed.tarkashvand@gmail.com>
2023-10-07 21:11:03 +0330

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const username = 'hamed'
+typeof username === 'string'
+
+if (!('serviceWorker' in navigator)) {
+  console.log('dsf')
+}
+const greeting = 'hello'
+console.log(`${greeting} world`)
+;[1, 2, 3].forEach(item => console.log(item))
---
+++ src/typescript-example.ts
@@ -0,0 +1,22 @@
+function add(a: number, b: number): number {
+  return a + b
+}
+const username = 'hamed'
+typeof username === 'string'
+interface User {
+  name: {
+    first: string
+    middle: string
+    last: string
+  }
+}
+function getFullName(user: User): string {
+  const {
+    name: {first, middle, last},
+  } = user
+  return [first, middle, last].filter(Boolean).join(' ')
+}
+
+add(1, 2)
+
+getFullName({name: {first: 2, middle: 'Bud', last: 'Matthews'}})
